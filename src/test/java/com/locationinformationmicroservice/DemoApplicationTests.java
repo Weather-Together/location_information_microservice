c54bd84817a28358ccdfd8b9d195bc96d55e75d5
Michael Kuhlmeier <kuhlmmb@gmail.com>
2024-02-10 13:01:58 -0700

    Get one passing test

--- src/test/java/com/locationinformationmicroservice/DemoApplicationTests.java
+++ src/test/java/com/locationinformationmicroservice/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.locationinformationmicroservice;
+package com.locationinformationmicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
