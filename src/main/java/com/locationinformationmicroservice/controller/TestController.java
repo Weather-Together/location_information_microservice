adc0afb6adbaef436737bb0be9c0ebdcfd0857fd
Blaine Kennedy <blainekennedy@gmail.com>
2024-02-27 18:30:08 -0700

    add a test controller that returns 10 images

--- src/main/java/com/locationinformationmicroservice/controller/TestController.java
+++ src/main/java/com/locationinformationmicroservice/controller/TestController.java
@@ -1,31 +1,40 @@
 package com.locationinformationmicroservice.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.locationinformationmicroservice.fixtures.ImageFixture;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import com.locationinformationmicroservice.fixtures.ImageFixture;
 
-
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v0")
 public class TestController {
-  
   @GetMapping("/test")
   @ResponseBody
-  public String location_information(@RequestParam String image_api_key, @RequestParam String query) { 
-    // String uri = "https://api.unsplash.com/" + "search/photos/?client_id=" + image_api_key + "&query=" + query;
+  public static List<String> getImages(@RequestParam String image_api_key, @RequestParam String query) {
+    // String uri = "https://api.unsplash.com/search/photos/?client_id=" + image_api_key + "&query=" + state;
     // RestTemplate restTemplate = new RestTemplate();
     // String result = restTemplate.getForObject(uri, String.class);
 
+    List<String> images = new ArrayList<>();
     String result = ImageFixture.getObject();
-    String image = result.split("\"raw\":\"")[1].split("\",\"full\":")[0];
-    
-    return image;   
+    Gson gson = new Gson();
+    JsonObject jsonResult = gson.fromJson(result, JsonObject.class);
+    JsonArray results = jsonResult.getAsJsonArray("results");
+
+    for (JsonElement element : results) {
+      JsonObject photo = element.getAsJsonObject();
+      JsonObject urls = photo.getAsJsonObject("urls");
+      String rawUrl = urls.get("raw").getAsString();
+      images.add(rawUrl);
+    }
+
+    return images;
   }
 }
-
-
-
-
